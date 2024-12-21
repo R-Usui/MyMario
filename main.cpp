49a26ec036968874bd5d7a83f324141433848aef
R-Usui <tumotumon_soft@outlook.com>
2024-12-21 23:04:00 +0900

    M: main.cpp (Changed to own sample main() from SFML official sample)

--- main.cpp
+++ main.cpp
@@ -1,24 +1,47 @@
+#include <iostream>
+
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Audio.hpp>
 
-int main()
+unsigned const int WINDOW_WIDTH = 800;
+unsigned const int WINDOW_HEIGHT = 600;
+
+int main(int argc, char* argv[])
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	sf::VideoMode mode = sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT);
+	auto tytle = "MyMario";
+	auto style = sf::Style::Close;
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	sf::RenderWindow window(mode, tytle, style);
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	window.setFramerateLimit(60);
 
-    return 0;
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+				 
+			case sf::Event::KeyPressed:
+
+				if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+				{
+					window.close();
+					break;
+				}
+				
+			default:
+				break;
+			}
+		}
+	}
+
+	return 0;
 }
